ba47acc5242c2e9700dffd9b50421f787a52eeb3
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2025-10-07 22:28:47 +0530

    frontend: adding two folders pages(Login,DashBoard) and componenets  (NavBar)

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,11 +1,11 @@
-import NxWelcome from './nx-welcome';
+
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="@tentwenty-timesheet/frontend" />
+      
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
